ab6536e8b28654b99078b1f95e440b28876d0ff5
qorbanisepehr <se.ghorbani@canymes.com>
2026-08-17 08:32:23 +0330

    fix(api): translate 403 AccessDeniedHttpException message
    - Add render callback for AccessDeniedHttpException in bootstrap/app.php
    - Returns translated messages.permission_denied instead of Symfony default

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -41,5 +42,11 @@ return Application::configure(basePath: dirname(__DIR__))
             return response()->json([
                 'message' => __('messages.not_found'),
             ], 404);
+        });
+
+        $exceptions->render(function (AccessDeniedHttpException $e, Request $request) {
+            return response()->json([
+                'message' => __('messages.permission_denied'),
+            ], 403);
         });
     })->create();
